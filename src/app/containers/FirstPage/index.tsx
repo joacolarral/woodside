bafe4caa5c426b1ddb7c9c9d0667fc59d6d3d370
jlarralde <Joaquin.Larralde@ice.com>
2024-11-04 14:42:49 -0300

    feat: added pictures to sections and taken out logo to first apge

--- src/app/containers/FirstPage/index.tsx
+++ src/app/containers/FirstPage/index.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Container from "@/app/components/Container";
-import Logo from "@/app/components/Logo";
+// import Logo from "@/app/components/Logo";
 import styles from "./FirstPage.module.scss";
 import Navbar from "@/app/components/Navbar";
 import Trip from "@/app/images/ws_trip.mp4";
@@ -11,9 +11,9 @@ export default function FirstPage() {
   return (
     <Container id="home" className={styles.container}>
       <Navbar />
-      <div className={styles.content}>
+      {/* <div className={styles.content}>
         <Logo />
-      </div>
+      </div> */}
       <video
         className={styles.video}
         src={Trip}
--- src/app/containers/Habitaciones/index.tsx
+++ src/app/containers/Habitaciones/index.tsx
@@ -5,16 +5,16 @@ import React, { useMemo } from "react";
 
 import Carousel from "@/app/components/Carousel";
 
-import img1 from "@/app/images/trip/trip_1.jpg";
-import img2 from "@/app/images/trip/trip_2.jpg";
-import img3 from "@/app/images/trip/trip_3.jpg";
-import img4 from "@/app/images/trip/trip_4.jpg";
-import img5 from "@/app/images/trip/trip_5.jpg";
+import img1 from "@/app/images/habitaciones/habitaciones-1.jpg";
+import img2 from "@/app/images/habitaciones/habitaciones-2.jpg";
+// import img3 from "@/app/images/trip/trip_3.jpg";
+// import img4 from "@/app/images/trip/trip_4.jpg";
+// import img5 from "@/app/images/trip/trip_5.jpg";
 // import useDeviceType from "@/app/hooks/useDeviceType";
 import Container from "@/app/components/Container";
 
 const buildSlides = () => {
-  const images = [img1, img2, img3, img4, img5];
+  const images = [img1, img2];
   return images.map((image, index) => ({
     id: index + 1,
     value: <Image key={index} fill src={image.src} alt="image" />,
